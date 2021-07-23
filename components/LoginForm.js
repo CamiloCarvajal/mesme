b58d1bc00bac9933176f849766d7e43e5b8b5cf5
Camilo Carvajal <camilo@talent.com>
2021-07-23 14:22:09 -0500

    login form

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Input, Button, Icon } from "react-native-elements";
 
 export default function LoginForm() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+  return (
+    <View>
+      <Input label="Email" placeholder="Ingresa tu email" />
+      <Input label="Contraseña" placeholder="Ingresa tu contraseña" />
+      <View style={{ flexDirection: "row" }}>
+        <Button title="Ingresar" />
+        <Button title="Crear cuenta" type="outline" />
+      </View>
+      <Text>Recuperar contraseña</Text>
+      <View style={{ flexDirection: "row" }}>
+        <Icon name="google" type="fontisto" color="#FF5733" size={20} reverse />
+        <Icon
+          name="facebook"
+          type="fontisto"
+          color="#2B58B4"
+          size={20}
+          reverse
+        />
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/Login.js
+++ screens/Login.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import LoginForm from "../components/LoginForm";
 
 export default function Login() {
-    return (
-        <View>
-            <Text style={{fontStyle:"italic", fontWeight:"bold"}}>MESME</Text>
-            <Text>Vestibulum et pretium diam. Nulla pellentesque mi nunc, et consectetur ipsum porta a. Nam in leo a urna egestas vestibulum non in nunc. Nunc volutpat congue gravida. Praesent ornare purus consectetur, lacinia felis a, tincidunt ante.</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>
+        Vestibulum et pretium diam. Nulla pellentesque mi nunc, et consectetur
+        ipsum porta a. Nam in leo a urna egestas vestibulum non in nunc. Nunc
+        volutpat congue gravida. Praesent ornare purus consectetur, lacinia
+        felis a, tincidunt ante.
+      </Text>
+      <LoginForm />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
