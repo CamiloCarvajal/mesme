73d42b731096da54207715abc839f24824edaddc
Camilo Carvajal <camilo@talent.com>
2021-07-23 13:33:22 -0500

    initial commit

---
+++ screens/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Login() {
+    return (
+        <View>
+            <Text style={{fontStyle:"italic", fontWeight:"bold"}}>MESME</Text>
+            <Text>Vestibulum et pretium diam. Nulla pellentesque mi nunc, et consectetur ipsum porta a. Nam in leo a urna egestas vestibulum non in nunc. Nunc volutpat congue gravida. Praesent ornare purus consectetur, lacinia felis a, tincidunt ante.</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ styles/Color.js
@@ -0,0 +1,7 @@
+export const Color = {
+    redLight: "#D66556",
+    redAccent: "#A34D42",
+    redAccent2: "#D66556" ,
+    redDark: "572923",
+    gay: "#666161"
+}
